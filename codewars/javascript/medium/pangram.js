002e609d8bcba95384d017d2553f36d72427eb38
Arthur Karambizi <arthurkarambizi@gmail.com>
2020-12-17 23:08:09 -0500

    made changes to the pangram challenge

--- codewars/javascript/medium/pangram.js
+++ codewars/javascript/medium/pangram.js
@@ -1,52 +1,40 @@
-/* A pangram is a sentence that contains every single letter of the alphabet at least once.For example, the sentence "The quick brown fox jumps over the lazy dog"
-is a pangram, because it uses the letters A - Z at least once(
-    case is irrelevant).
+/*
+ ** Source : https://www.codewars.com/kata/54da5a58ea159efa38000836
+ ** Date   : 12-16-2020
+ */
 
-Given a string, detect whether or not it is a pangram.Return True
-if it is, False
-if not.Ignore numbers and punctuation.
+/*
+ ** A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+ **
+ ** Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+ */
+
+/**
+ * @param {string} string
+ * @return {boolean}
  */
 function isPangram(string) {
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    let alphabetSorted = alphabet
-        .toLowerCase()
-        .split('')
-        .sort();
-    let stringSorted = string
-        .toLowerCase()
-        .split('')
-        .sort();
-    let stringSortedNoRepeat = stringSorted.filter((value, index, array) => {
-        return array.indexOf(value) == index;
-    });
+    const alphabets = { a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0, q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0 };
+    const stringLowerCase = string.toLowerCase();
+    let output = true;
 
-    if (stringSortedNoRepeat.join('').includes(alphabetSorted.join(''))) {
-        return true;
-    } else {
-        return false;
+    for (let index = 0; index < stringLowerCase.length; index++) {
+        const letter = stringLowerCase[index];
+
+        if (letter in alphabets) {
+            alphabets[letter] += 1;
+        }
     }
-}
-let string = 'The quick brown fox jumps over the lazy dog.';
-isPangram(string);
 
-//--------------------------------------------------------------------------------
-//other solutions
+    for (const letter in alphabets) {
+        if (alphabets[letter] === 0) {
+            output = false;
+            break;
+        }
+    }
 
-function isPangram(string) {
-    string = string.toLowerCase();
-    return "abcdefghijklmnopqrstuvwxyz".split("").every(function (x) {
-        return string.indexOf(x) !== -1;
-    });
+    return output;
 }
 
-//--------------------------------------------------------------------------------
-function isPangram(string) {
-    return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
-}
-
-//--------------------------------------------------------------------------------
-function isPangram(string) {
-    return "abcdefghijklmnopqrstuvwxyz".split('').every(function (e) {
-        return string.toLowerCase().indexOf(e) >= 0;
-    });
-}
+isPangram('The quick brown fox jumps over the lazy dog.'); // true
+isPangram('Pack my box with five dozen liquor jugs.'); // true
