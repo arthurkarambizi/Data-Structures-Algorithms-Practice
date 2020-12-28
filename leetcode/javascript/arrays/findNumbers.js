17c2ecbeb01d0889024f734c466601ab1bb5c04c
Arthur Karambizi <arthurkarambizi@gmail.com>
2020-12-28 15:55:11 -0800

    made changes to the findMaxConsecutiveOnes

--- leetcode/javascript/arrays/findNumbers.js
+++ leetcode/javascript/arrays/findNumbers.js
@@ -1,53 +1,53 @@
 /*
-** Source : https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
-** Date   : 11-01-2020
-*/
+ ** Source : https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+ ** Date   : 12-28-2020
+ */
 
 /*
-** Given an array nums of integers, return how many of them contain an even number of digits.
-**
-**
-** Example 1:
-**
-** Input: nums = [12,345,2,6,7896]
-** Output: 2
-** Explanation:
-** 12 contains 2 digits (even number of digits).
-** 345 contains 3 digits (odd number of digits).
-** 2 contains 1 digit (odd number of digits).
-** 6 contains 1 digit (odd number of digits).
-** 7896 contains 4 digits (even number of digits).
-** Therefore only 12 and 7896 contain an even number of digits.
-** Example 2:
-**
-** Input: nums = [555,901,482,1771]
-** Output: 1
-** Explanation:
-** Only 1771 contains an even number of digits.
-**
-**
-** Constraints:
-**
-** 1 <= nums.length <= 500
-** 1 <= nums[i] <= 10^5
-*/
+ ** Given an array nums of integers, return how many of them contain an even number of digits.
+ **
+ **
+ ** Example 1:
+ **
+ ** Input: nums = [12,345,2,6,7896]
+ ** Output: 2
+ ** Explanation:
+ ** 12 contains 2 digits (even number of digits).
+ ** 345 contains 3 digits (odd number of digits).
+ ** 2 contains 1 digit (odd number of digits).
+ ** 6 contains 1 digit (odd number of digits).
+ ** 7896 contains 4 digits (even number of digits).
+ ** Therefore only 12 and 7896 contain an even number of digits.
+ ** Example 2:
+ **
+ ** Input: nums = [555,901,482,1771]
+ ** Output: 1
+ ** Explanation:
+ ** Only 1771 contains an even number of digits.
+ **
+ **
+ ** Constraints:
+ **
+ ** 1 <= nums.length <= 500
+ ** 1 <= nums[i] <= 10^5
+ */
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
-var findNumbers = function (nums) {
+function findNumbers(nums) {
     let output = 0;
+
     for (let index = 0; index < nums.length; index++) {
         const numberDigits = String(nums[index]).length;
         if (numberDigits % 2 === 0) {
             output++;
         }
-	}
+    }
 
     return output;
-};
+}
 
-let nums = [12, 345, 2, 6, 7896]; // output 2
-// let nums = [555,901,482,1771,11] // output 1
-findNumbers(nums)
+findNumbers([12, 345, 2, 6, 7896]); // 2
+findNumbers([555, 901, 482, 1771, 11]); // 1
--- leetcode/javascript/arrays/merge.js
+++ leetcode/javascript/arrays/merge.js
@@ -1,48 +1,48 @@
 /*
-** Source : https://leetcode.com/problems/merge-sorted-array/
-** Date   : 11-01-2020
-*/
-
+ ** Source : https://leetcode.com/problems/merge-sorted-array/
+ ** Date   : 12-28-2020
+ */
 
 /*
-**
-** Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
-**
-** Note:
-**
-** The number of elements initialized in nums1 and nums2 are m and n respectively.
-** You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
-** Example:
-**
-** Input:
-** nums1 = [1,2,3,0,0,0], m = 3
-** nums2 = [2,5,6],       n = 3
-**
-** Output: [1,2,2,3,5,6]
-**
-**
-** Constraints:
-**
-** -10^9 <= nums1[i], nums2[i] <= 10^9
-** nums1.length == m + n
-** nums2.length == n
-**
-*/
+ **
+ ** Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ **
+ ** Note:
+ **
+ ** The number of elements initialized in nums1 and nums2 are m and n respectively.
+ ** You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+ ** Example:
+ **
+ ** Input:
+ ** nums1 = [1,2,3,0,0,0], m = 3
+ ** nums2 = [2,5,6],       n = 3
+ **
+ ** Output: [1,2,2,3,5,6]
+ **
+ **
+ ** Constraints:
+ **
+ ** -10^9 <= nums1[i], nums2[i] <= 10^9
+ ** nums1.length == m + n
+ ** nums2.length == n
+ **
+ */
 /**
  * @param {number[]} nums1
  * @param {number} m
  * @param {number[]} nums2
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
+ * TODO: refactor
  */
-var merge = function (nums1, m, nums2, n) {
+function merge(nums1, m, nums2, n) {
     for (let index = 0; index < nums2.length; index++) {
         const element = nums2[index];
         nums1.splice(nums1.length - (index + 1), 1); // remove empty space from the array
         nums1.push(element); // add nums element
     }
     nums1.sort((a, b) => a - b);
-};
+}
 
 let nums1 = [1, 2, 3, 0, 0, 0];
 let m = 3;
